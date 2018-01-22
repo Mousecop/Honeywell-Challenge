84f4bf07a6504d389134577530f2db069ada421c
Mousecop <joaskew13@gmail.com>
2018-01-22 12:00:59 -0500

    adds functionality for left nav menu

---
+++ app.js
@@ -0,0 +1,71 @@
+var testQuerySelector = function() {
+    var homeIcon = document.querySelector('.home');
+    console.log(homeIcon.classList)
+};
+
+var toggleActiveClass = function() {
+    var homeElement = document.querySelector('.home');
+    var dashElement = document.querySelector('.dashboard');
+    var servicesElement =  document.querySelector('.services');
+
+    homeElement.addEventListener('click', function(e) {
+        // e.preventDefault();
+
+        if(!(this.classList.contains('active-nav-item'))) {
+            this.classList.add('active-nav-item')
+        } else if(dashElement.classList.contains('active-nav-item')) {
+            dashElement.classList.remove('active-nav-item')
+        } else if(servicesElement.classList.contains('active-nav-item')) {
+            servicesElement.classList.remove('active-nav-item')
+        }
+    })
+
+
+    dashElement.addEventListener('click', function(e) {
+        // e.preventDefault();
+
+        if(!(this.classList.contains('active-nav-item'))) {
+            this.classList.add('active-nav-item')
+        } else if(homeElement.classList.contains('active-nav-item')) {
+            homeElement.classList.remove('active-nav-item')
+        } else if(servicesElement.classList.contains('active-nav-item')) {
+            servicesElement.classList.remove('active-nav-item')
+        }
+    })
+
+    servicesElement.addEventListener('click', function(e) {
+        // e.preventDefault();
+
+        if(!(this.classList.contains('active-nav-item'))) {
+            this.classList.add('active-nav-item')
+        } else if(dashElement.classList.contains('active-nav-item')) {
+            dashElement.classList.remove('active-nav-item')
+        } else if(homeElement.classList.contains('active-nav-item')) {
+            homeElement.classList.remove('active-nav-item')
+        }
+    })
+}
+
+var toggleLeftNav = function() {
+    var caretElement = document.querySelector('.caret');
+    var leftNavUl = document.querySelector('.left-nav-ul');
+
+    caretElement.addEventListener('click', function(e) {
+        leftNavUl.classList.toggle('toggle-left-nav-window');
+        caretElement.classList.toggle('caret-rotate');
+
+        // if(leftNavUl.classList.contains('toggle-left-nav-window')) {
+        //     leftNavUl.classList.toggle('toggle-left-nav-window')
+        //     caretElement.classList.toggle('caret-rotate')
+        // } else {
+        //     leftNavUl.classList.add('toggle-left-nav-window')
+        //     caretElement.classList.add('caret-rotate')
+        // }
+    })
+}
+
+
+//Invocations
+toggleLeftNav();
+toggleActiveClass();
+testQuerySelector();
