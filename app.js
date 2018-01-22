166ffdb23f298f473341442db707c4b166cc7eb1
Mousecop <joaskew13@gmail.com>
2018-01-22 14:09:09 -0500

    Builds out profile image click logic

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-var testQuerySelector = function() {
-    var homeIcon = document.querySelector('.home');
-    console.log(homeIcon.classList)
-};
-
 var toggleActiveClass = function() {
     var homeElement = document.querySelector('.home');
     var dashElement = document.querySelector('.dashboard');
@@ -50,8 +45,58 @@ var toggleLeftNav = function() {
     })
 }
 
+var toggleProfileImageClick = function() {
+    var profileImage = document.querySelector('.profile-image');
+    var xIcon = document.querySelector('.x-icon');
+    var containerElement = document.querySelector('.profile-container');
+    var profileName = document.querySelector('.profile-name');
+    var dropDownDrawer = document.querySelector('.dropdown-menu-ul');
+    var dropDownItems = document.querySelectorAll('.dropdown-item');
+
+    profileImage.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        this.classList.add('profile-image-click');
+        xIcon.classList.add('x-icon-toggle');
+        containerElement.classList.add('profile-container-toggle');
+        profileName.classList.add('profile-name-toggle');
+        dropDownDrawer.classList.add('dropdown-menu-ul-toggle');
+
+        //Loop to add class to all dropdown menu items
+        for(var i =0; i<dropDownItems.length; i++) {
+            dropDownItems[i].classList.add('dropdown-item-toggle');
+        }
+
+        //Loop to stagger sliding transition
+        for(var i =0; i<dropDownItems.length; i++) {
+            dropDownItems[i].classList.add('slide-item-toggle');
+        }
+
+    })
+
+    xIcon.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        this.classList.remove('x-icon-toggle');
+        profileImage.classList.remove('profile-image-click');
+        containerElement.classList.remove('profile-container-toggle');
+        profileName.classList.remove('profile-name-toggle');
+        dropDownDrawer.classList.remove('dropdown-menu-ul-toggle');
+
+        //Loop to remove class to all dropdown menu items
+        for(var i =0; i<dropDownItems.length; i++) {
+            dropDownItems[i].classList.remove('dropdown-item-toggle');
+        }
+
+        //Loop to remove stagger sliding transition
+        for(var i =0; i<dropDownItems.length; i++) {
+            dropDownItems[i].classList.remove('slide-item-toggle');
+        }
+    })
+}
+
 
 //Invocations
+toggleProfileImageClick();
 toggleLeftNav();
 toggleActiveClass();
-testQuerySelector();
